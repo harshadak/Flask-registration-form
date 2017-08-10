a4e5dbc60ecb05b2771a8325ee2ef20b5797e087
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-08-10 02:36:10 -0700

    Finished the Ninja version. Need to check on flash category styling next.

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, session, flash
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 NAME_REGEX = re.compile(r'[0-9]')
+PASS_REGEX = re.compile(r'.*[A-Z].*[0-9]')
 
 app = Flask(__name__)
 app.secret_key = "Secret"
@@ -22,21 +23,24 @@ def process():
 
     for i in request.form:
         if len(request.form[i]) < 1:
-            flash("This field cannot be blank!")
+            flash(i + " field cannot be blank!", "error")
 
     if NAME_REGEX.match(first_val) or NAME_REGEX.match(last_val):
-        flash("Your name cannot contain any numbers")
-    elif password_val < 8:
-        flash("Password should be more than 8 characters long.")
-    elif not EMAIL_REGEX.match(email_val):
-        flash("Invalid Email Address!")
-    elif password_val != confirm_val:
-        flash("Your passwords didn't match. Please try again.")
+        flash("Your name cannot contain any numbers", "error")
+    if len(password_val) < 8 or len(confirm_val) < 8:
+        flash("Password should be more than 8 characters long.", "error")
+    if not EMAIL_REGEX.match(email_val):
+        flash("Invalid Email Address!", "email")
+    if not PASS_REGEX.match(password_val):
+        flash("Password must have at least 1 uppercase letter and 1 numeric value.", "email")
+    if password_val != confirm_val:
+        flash("Your passwords didn't match. Please try again.", "email")
     
     
     if '_flashes' in session: 
         return render_template("index.html")
     else:
+        flash("Thanks for submitting your information.", "success")
         return redirect("/")
 
 app.run(debug = True)
